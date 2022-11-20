62a4a7f26e2f46c0dfcb4652be73d85a781f55ff
Major4ik <2772771882@mail.ru>
2022-11-20 20:59:09 +0300

    fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open(r'/Users/aleksejzuravlev/PycharmProjects/vk-a/README.md', 'r', encodin
 
 setuptools.setup(
 	name='vka',
-	version='0.1',
+	version='1.0',
 	author='Major4ik',
 	author_email='2772771882@mail.ru',
 	description='module for the vk api wrapper',
@@ -17,5 +17,5 @@ setuptools.setup(
 		"License :: OSI Approved :: MIT License",
 		"Operating System :: OS Independent",
 	],
-	python_requires='>=3.6',
+	python_requires='>=3.10',
 )
