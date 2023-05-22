b58b03b1dff4848d80b8ce34f8643ee8cd65b261
Алексей  Журавлев <2772771882@mail.ru>
2023-05-22 21:46:19 +0300

    fix

--- vka/chatbot/registration.py
+++ vka/chatbot/registration.py
@@ -41,18 +41,48 @@ class Registration:
             if s.find('ya:location') is not None \
             else s.find('ya:location')
 
+        public_access = s.find('ya:publicaccess').text \
+            if s.find('ya:publicaccess') is not None else None
+
+        profile_state = s.find('ya:profilestate').text \
+            if s.find('ya:profilestate') is not None else None
+
+        user_id = s.find('ya:uri').attrs.get('rdf:resource') \
+            if s.find('ya:uri') is not None else None
+
+        first_name = s.find('ya:firstname').text \
+            if s.find('ya:firstname') is not None else None
+
+        second_name = s.find('ya:secondname').text \
+            if s.find('ya:secondname') is not None else None
+
+        gender = s.find('foaf:gender').text \
+            if s.find('foaf:gender') is not None else None
+
+        subscribers_count = s.find('ya:subscriberscount').text \
+            if s.find('ya:subscriberscount') is not None else None
+
+        friends_count = s.find('ya:friendscount').text \
+            if s.find('ya:friendscount') is not None else None
+
+        subscribed_to_count = s.find('ya:subscribedtocount').text \
+            if s.find('ya:subscribedtocount') is not None else None
+
+        birthday = s.find('foaf:birthday').text \
+            if s.find('foaf:birthday') is not None else None
+
         return {
-            "public_access": s.find('ya:publicaccess').text,
-            "profile_state": s.find('ya:profilestate').text,
-            "id": s.find('ya:uri').attrs.get('rdf:resource'),
-            "first_name": s.find('ya:firstname').text,
-            "second_name": s.find('ya:secondname').text,
-            "name": s.find('foaf:name').text,
-            "gender": s.find('foaf:gender').text,
-            "subscribers_count": s.find('ya:subscriberscount').text,
-            "friends_count": s.find('ya:friendscount').text,
-            "subscribed_to_count": s.find('ya:subscribedtocount').text,
-            "birthday": s.find('foaf:birthday').text,
+            "public_access": public_access,
+            "profile_state": profile_state,
+            "id": user_id,
+            "first_name": first_name,
+            "second_name": second_name,
+            "name": f"{first_name} {second_name}",
+            "gender": gender,
+            "subscribers_count": subscribers_count,
+            "friends_count": friends_count,
+            "subscribed_to_count": subscribed_to_count,
+            "birthday": birthday,
             "location": location,
             "created": datetime.strptime(
                 s.find('ya:created').attrs.get('dc:date'),
